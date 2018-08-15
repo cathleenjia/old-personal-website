1f1617c3176e7eb9a389ceca11ade72b615620c7
Cathleen Jia <cathleenjia@berkeley.edu>
2018-08-15 02:29:16 -0400

    hmf

--- btf-new/main.js
+++ btf-new/main.js
@@ -21,3 +21,16 @@ function scrollToStaff() {
 function scrollToPolicies() {
   document.getElementById('policies-page').scrollIntoView(true);
 }
+
+function menuControls(){
+  if (!isHidden(document.querySelector('.hamburger-menu'))) {
+    document.querySelector('.hamburger-menu').setAttribute('style', 'display:none;');
+  } else {
+    document.querySelector('.hamburger-menu').setAttribute('style', 'display:block;');
+  }
+}
+
+function isHidden(el) {
+    var style = window.getComputedStyle(el);
+    return (style.display === 'none')
+}
